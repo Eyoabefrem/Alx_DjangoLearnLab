cd969ecb9d9b2da294df935f5da7dc27ef203a30
Eyoab Tessera <eyoabefrem9@gmail.com>
2024-11-03 01:05:06 +0300

    django

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 class Book(models.Model):
+    STATUS_CHOICES = [
+        ('a', 'Available'),
+        ('o', 'On loan'),
+        ('r', 'Reserved'),
+        ('m', 'Maintenance'),
+    ]
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+    Due_back = models.DateField(default=timezone.now)
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        blank=True,
+        default='m',
+        help_text='Book availiability'
+    )
 
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+    def str(self):
+        return f"{self.title} by {self.author}"
